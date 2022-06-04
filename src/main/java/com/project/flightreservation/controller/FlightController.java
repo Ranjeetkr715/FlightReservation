3ede612d943ea86295cbfedc16c9e1b3a0309f78
suchit17 <34216149+suchit17@users.noreply.github.com>
2022-06-04 10:47:37 +0530

    Updated FlightController

--- src/main/java/com/project/flightreservation/controller/FlightController.java
+++ src/main/java/com/project/flightreservation/controller/FlightController.java
@@ -1,40 +1,37 @@
 package com.project.flightreservation.controller;
 
 
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
-
+import com.project.flightreservation.entities.Flight;
+import com.project.flightreservation.repo.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.project.flightreservation.entities.Flight;
-import com.project.flightreservation.repo.FlightRepository;
+import java.sql.Date;
+import java.util.List;
 
 
 @Controller
 public class FlightController {
 
-	@Autowired
-	private FlightRepository  flightRepository;
+  @Autowired
+  private FlightRepository flightRepository;
 
 
-	@RequestMapping("findFlights")
-	public String findFlights(@RequestParam("from") String from, @RequestParam("to") String to,
-			@RequestParam("departureDate") @DateTimeFormat(pattern = "MM-dd-yyyy") Date departureDate,
-			ModelMap modelMap) {
-		
-
-		List<Flight> flights = flightRepository.findFlights(from, to, departureDate);
-		
-		modelMap.addAttribute("flights", flights);
+  @RequestMapping("findFlights")
+  public String findFlights(@RequestParam("from") String from, @RequestParam("to") String to,
+                            @RequestParam("departureDate") @DateTimeFormat(pattern = "MM-dd-yyyy") String departureDate,
+                            ModelMap modelMap) {
 
 
-		return "displayFlights";
+    List<Flight> flights = flightRepository.findFlights(from.toUpperCase(), to.toUpperCase(), Date.valueOf(departureDate));
 
-	}
+    modelMap.addAttribute("flights", flights);
+
+
+    return "displayFlights";
+
+  }
 }
