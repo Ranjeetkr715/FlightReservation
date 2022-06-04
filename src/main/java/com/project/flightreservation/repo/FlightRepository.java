61fdf346a52fb5bd25078a00601815841037b9c3
suchit17 <34216149+suchit17@users.noreply.github.com>
2022-06-04 10:48:49 +0530

    Updated Filerepository

--- src/main/java/com/project/flightreservation/repo/FlightRepository.java
+++ src/main/java/com/project/flightreservation/repo/FlightRepository.java
@@ -1,18 +1,15 @@
 package com.project.flightreservation.repo;
 
-import java.util.Date;
-import java.util.List;
-
+import com.project.flightreservation.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.sql.Date;
+import java.util.List;
 
-import com.project.flightreservation.entities.Flight;
+public interface FlightRepository extends JpaRepository<Flight, Long> {
 
-public interface FlightRepository extends JpaRepository<Flight,Long> {
-
-	@Query("from Flight where departureCity=:departureCity and ArrivalCity=:ArrivalCity and dateOfDeparture=:dateOfDeparture")
-	List<Flight>  findFlights(@Param("departureCity") String from,@Param("ArrivalCity") String to,@Param("dateOfDeparture") Date departureDate);
-
+  @Query("from Flight where departureCity=:departureCity and ArrivalCity=:ArrivalCity and dateOfDeparture=:departureDate")
+  List<Flight> findFlights(@Param("departureCity") String from, @Param("ArrivalCity") String to, @Param("dateOfDeparture") Date departureDate);
 
 }
